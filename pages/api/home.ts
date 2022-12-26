cd43bc08aa486e6d22ff79808c583b4b49449b69
galaraz <jvictor.si.2013@gmail.com>
2022-12-26 17:52:36 -0400

    api teste

---
+++ pages/api/home.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from 'next/server'
+import {apiUrl,apiId} from '../../utils';
+
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function (req: NextRequest) {
+  
+
+    const corpo = await JSON.stringify( {
+        acoes: [                        
+          { metodo: "destaques", params: [ { resultados: "4" }] },
+          { metodo: "ultimasnoticias", params: [ { resultados: "4" }] },
+        ], id: apiId
+      });
+    
+    const response =  await fetch(
+        apiUrl,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: corpo
+        }
+    
+    );
+    const list = await response.json()
+  
+    return new Response (JSON.stringify(list)) 
+}
