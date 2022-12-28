34bc8ceb34f2e9c37980143bdd94dc7e077bd87a
galaraz <jvictor.si.2013@gmail.com>
2022-12-28 10:06:01 -0400

    sem contexto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,16 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthProvider from '../context';
-import Header from '../components/Header';
-// import Footer from '../components/Footer'
+
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Header/>
+    
+      
       <Component {...pageProps} />
       
-    </AuthProvider>
-  )
+    )
   
   
 }
