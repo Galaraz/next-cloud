c95adfae698d99e478db5a21d80e13666acb6b9b
galaraz <jvictor.si.2013@gmail.com>
2022-12-28 10:59:46 -0400

    components novament

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// import AuthProvider from '../context';
+import Header from '../components/Header';
+// import Footer from '../components/Footer'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    
-      
+    <>
+      <Header/>
       <Component {...pageProps} />
-      
-    )
+    </>
+  )
   
   
 }
